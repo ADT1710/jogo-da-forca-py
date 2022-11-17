f8b0ec9abe8263231fcb66001a9a213fb7479187
adt1710 <adrianoibira.adt@gmail.com>
2022-11-17 07:10:57 -0300

    Validação da entrada do usuário de acordo com a palavra gerada

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from palavras import palavras
+import string
 
 def pega_palavra_valida(palavras):
     palavra = random.choice(palavras)
@@ -7,3 +8,22 @@ def pega_palavra_valida(palavras):
         word = random.choice(palavras)
 
     return palavra
+
+def jogo_da_forca():
+    palavra = pega_palavra_valida(palavras)
+    letras_palavra = set(palavra)  # Letras na palavra
+    alfabeto = set(string.ascii_uppercase)
+    letras_utilizadas = set()  # Letras que o usuário advinhou
+
+    # Pegando a entrada do usuário
+    letra_do_usuario = input("Chute uma letra: ").upper()
+    if letra_do_usuario in alfabeto - letras_utilizadas:
+        letras_utilizadas.add(letra_do_usuario)
+        if letra_do_usuario in letras_palavra:
+            letras_palavra.remove(letra_do_usuario)
+
+    elif letra_do_usuario in letras_utilizadas:
+        print("Você já utilizou essa letra. Tente uma nova.")
+
+    else:
+        print('Caractere inválido. Tente novamente!')
