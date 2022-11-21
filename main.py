f8575c9010e50581d15b337447611429c592c1be
adt1710 <adrianoibira.adt@gmail.com>
2022-11-21 07:10:59 -0300

    Projeto funcional - necessita de correção de bugs

--- main.py
+++ main.py
@@ -4,26 +4,41 @@ import string
 
 def pega_palavra_valida(palavras):
     palavra = random.choice(palavras)
-    while '-' in palavra or '' in palavra:
-        word = random.choice(palavras)
+    while '-' in palavra or ' ' in palavra:
+        palavra = random.choice(palavras)
+
 
     return palavra
 
 def jogo_da_forca():
     palavra = pega_palavra_valida(palavras)
-    letras_palavra = set(palavra)  # Letras na palavra
+    print(palavra)
+    letras_palavra = set(palavra.upper())  # Letras na palavra
+    print(letras_palavra)
     alfabeto = set(string.ascii_uppercase)
     letras_utilizadas = set()  # Letras que o usuário advinhou
 
     # Pegando a entrada do usuário
-    letra_do_usuario = input("Palpite a letra desejada: ").upper()
-    if letra_do_usuario in alfabeto - letras_utilizadas:
-        letras_utilizadas.add(letra_do_usuario)
-        if letra_do_usuario in letras_palavra:
-            letras_palavra.remove(letra_do_usuario)
+    while len(letras_palavra) > 0:
+        # Letras utilizadas
+        print('Você já utilizou essas letras: ', ' '.join(letras_utilizadas))
 
-    elif letra_do_usuario in letras_utilizadas:
-        print("Você já utilizou essa letra. Tente uma nova.")
+        # Estado atual da palavra adivinhada
+        lista_de_letras = [letra if letra in letras_utilizadas else '-' for letra in palavra]
+        print(lista_de_letras)
+        print('Estado atual da palavra: ', ' '.join(lista_de_letras))
 
-    else:
-        print('Caractere inválido. Tente novamente!')
+        letra_do_usuario = input("Palpite a letra desejada: ").upper()
+        if letra_do_usuario in alfabeto - letras_utilizadas:
+            letras_utilizadas.add(letra_do_usuario)
+            if letra_do_usuario in letras_palavra:
+                letras_palavra.remove(letra_do_usuario)
+
+
+        elif letra_do_usuario in letras_utilizadas:
+            print("Você já utilizou essa letra. Tente uma nova.")
+
+        else:
+            print('Caractere inválido. Tente novamente!')
+
+jogo_da_forca()
